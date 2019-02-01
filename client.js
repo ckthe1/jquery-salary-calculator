8aa8c72da030b15be957d1dd3dc9913436c1c39b
Ck T <chengkou@gmail.com>
2019-02-01 17:42:36 -0600

    append new employee, clear

--- client.js
+++ client.js
@@ -10,20 +10,25 @@ function onReady(){
 }//end onReady
 function addInput(){
     console.log('input values');
-    
     let inputFirstName = $('#inputFirstName').val();
     console.log(inputFirstName);
-    
     let inputLastName = $('#inputLastName').val();
     console.log(inputLastName);
-    
     let inputId = $('#inputId').val();
     console.log(inputId);
-    
     let inputTitle = $('#inputTitle').val();
     console.log(inputTitle);
-   
     let inputAnnualSalary = $('#inputSalary').val();
     console.log(inputAnnualSalary);
     
+    $("#employeesBody").append(`
+    <tr class=employeeInfos>
+                <td>${inputFirstName}</td>
+                <td>${inputLastName}</td>
+                <td>${inputId}</td>
+                <td>${inputTitle}</td>
+                <td>${inputAnnualSalary}</td>
+            </tr>
+    `)
+    $('.inputs').val('');
 }
