159ddb299980a3e0d1e18557697b1e8ea5f46b01
Ck T <chengkou@gmail.com>
2019-02-03 18:02:39 -0600

    add comments

--- client.js
+++ client.js
@@ -3,8 +3,8 @@ $(document).ready(onReady);
 
 function onReady(){
     console.log('jQuery');
-    $('#submitButton').on('click', addInput);
-    $('.tableBody').on('click', '.deleteButton', deleteEmployee);
+    $('#submitButton').on('click', addInput);// on click of the submit button run addInput
+    $('.tableBody').on('click', '.deleteButton', deleteEmployee);//
 
 }//end onReady
 let totalCost = 0; 
@@ -25,12 +25,13 @@ function addInput(){
     console.log(inputAnnualSalary);
 
     let monthlyCost= totalCost/12;//monthly cost
-    $('#totalMonthlyCostOut').text(`Total Monthly: ${monthlyCost}`);
+    $('#totalMonthlyCostOut').text(`Total Monthly: ${monthlyCost}`);//replace totalMonthlyCostOut with Total Monthly: "monthly cost value"
+
     if (monthlyCost > 20000){
-        $('#totalMonthlyCostOut').css('background-color', 'red');
+        $('#totalMonthlyCostOut').css('background-color', 'red');//use css to change back ground color to red in totalMonthlyCostOut
     }//end if statement
 
-    $('.tableBody').append(`
+    $('.tableBody').append(` 
     <tr class=employeeInfos>
         <td id= first>${inputFirstName} </td>
         <td id= last>${inputLastName} </td>
@@ -39,16 +40,15 @@ function addInput(){
         <td id= salary>${inputAnnualSalary} </td>
         <td> <button class="deleteButton">Delete</button></td>
     </tr>
-    `)
-    $('.inputs').val('');
+    `)// append this row to the tableBody
+
+    $('.inputs').val('');//empty the values, class = inputs 
 }//end addInput funtion
-
-
 
 function deleteEmployee() {
     console.log('delete Employee');
-    $(this).closest('tr').remove();//remove row of employee, closest tr
-
+    $(this).closest('tr').remove();//remove row of employee.
+    // (this), the thing that was just clicked, target 'tr', remove it
 }//end deleteEmployee
 
 
